43b1115625865672255449d4091db18775cc1a7b
Robert Horton Jr <Hortonjrdesigns@gmail.com>
2021-05-13 17:01:57 -0400

    Initial commit

---
+++ 2D basic Animation/Assets/Hero/Scripts/Hero_Controller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero_Controller : MonoBehaviour {
+    public  float moveSpeed;
+    public Animator myAnime;
+
+	// Use this for initialization
+	void Start () 
+    {
+        myAnime = this.GetComponent<Animator>();	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if(Input.GetAxisRaw("Horizontal")<0)
+        //if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            // Set animation to go left
+            myAnime.SetInteger("DIR", 2);
+            transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
+        }
+         else if(Input.GetAxisRaw("Horizontal")>0)
+        //if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            // Set animation to go right
+            myAnime.SetInteger("DIR", 0);
+            transform.Translate(Vector2.right * Time.deltaTime * moveSpeed);
+        }
+        else if (Input.GetAxisRaw("Vertical")<0)
+
+        {
+            // Set animation to go down
+            myAnime.SetInteger("DIR", 3);
+        }
+       else if (Input.GetAxisRaw("Vertical")>0)
+        {
+            // Set animation to go up
+            myAnime.SetInteger("DIR", 1);
+        }
+
+	}
+}
---
+++ 2D basic Animation/Assets/Hero/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    Animator anim;
+    SpriteRenderer render;
+    Rigidbody2D rigid;
+    public bool controls = true;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        render = GetComponent<SpriteRenderer>();
+        rigid = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f){
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+        }
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f){
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+        }
+
+        if(Input.GetAxisRaw("Horizontal") > 0){
+            anim.SetBool("MoveX", true);
+            anim.SetBool("MoveUp", false);
+            anim.SetBool("MoveDown", false);
+            render.flipX = true;
+            anim.speed = 1;
+        } else if (Input.GetAxisRaw("Horizontal") < 0){
+            anim.SetBool("MoveX", true);
+            anim.SetBool("MoveUp", false);
+            anim.SetBool("MoveDown", false);
+            render.flipX = false;
+            anim.speed = 1;
+        } else if (Input.GetAxisRaw("Vertical") < 0){
+            anim.SetBool("MoveX", false);
+            anim.SetBool("MoveUp", false);
+            anim.SetBool("MoveDown", true);
+            anim.speed = 1;
+        } else if (Input.GetAxisRaw("Vertical") > 0){
+            anim.SetBool("MoveX", false);
+            anim.SetBool("MoveUp", true);
+            anim.SetBool("MoveDown", false);
+            anim.speed = 1;
+        } else {
+            anim.speed = 0;
+        }
+    }
+}
